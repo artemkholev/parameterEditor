82ce9af324d9d1c237606bf85d341669749ccc37
Artem <artemkholev@yandex.ru>
2023-11-07 23:31:08 +0300

    add lists and toggle-task

--- src/pages/getPars/index.tsx
+++ src/pages/getPars/index.tsx
@@ -1,18 +1,20 @@
-import { Layout, Row, Typography } from "antd";
+import { Col, Empty, Layout, Row, Spin, Typography } from "antd";
 // import { variant, list } from "@effector/reflect";
 // import { combine } from "effector";
 
 import { TasksFilters } from "../../features/tasks-filters";
-// import { ToggleTask } from "features/toggle-task";
+import { ToggleTask } from "../../features/toggle-task";
 // import { TaskRow, taskModel } from "entities/task";
 import styles from "./styles.module.scss";
+import { list, variant } from "@effector/reflect";
+import { combine } from "effector";
+import { TaskRow, taskModel } from "../../entities/task";
 
 
 export const GetPars = () => {
   return (
     <Layout className={styles.root}>
       <Layout className={styles.toolbar}>
-        {/* ~ Layout.Toolbar */}
         <Row justify="center">
           <Typography.Title level={1}>Reader file</Typography.Title>
         </Row>
@@ -22,53 +24,53 @@ export const GetPars = () => {
       </Layout>
       <Layout.Content className={styles.content}>
         <Row gutter={[0, 20]} justify="center">
-          {/* <PageContent /> */}
+          <PageContent />
         </Row>
       </Layout.Content>
     </Layout>
   );
 }
 
-// const ListItemView: React.FC<{ task: import("shared/api").Task }> = ({ task }) => {
-//   return (
-//     <Col key={task.id} span={24}>
-//       <TaskRow
-//         data={task}
-//         titleHref={`/${task.id}`}
-//         before={<ToggleTask taskId={task.id} withStatus={false} />}
-//       />
-//     </Col>
-//   );
-// };
+const ListItemView: React.FC<{ task: import("../../shared/api").Task }> = ({ task }) => {
+  return (
+    <Col key={task.id} span={24}>
+      <TaskRow
+        data={task}
+        titleHref={`/${task.id}`}
+        before={<ToggleTask taskId={task.id} withStatus={false} />}
+      />
+    </Col>
+  );
+};
 
-// Использование effector-reflect здесь опционально и некритично в рамках методологии
-// const TasksList = list({
-//   view: ListItemView,
-//   source: taskModel.$tasksFiltered,
-//   bind: {},
-//   mapItem: {
-//     task: (task) => task,
-//   },
-// });
-// Использование effector-reflect здесь опционально и некритично в рамках методологии
-// const PageContent = variant({
-//   source: combine(
-//     {
-//       isLoading: taskModel.$tasksListLoading,
-//       isEmpty: taskModel.$tasksListEmpty,
-//     },
-//     ({ isLoading, isEmpty }) => {
-//       if (isLoading) return "loading";
-//       if (isEmpty) return "empty";
-//       return "ready";
-//     }
-//   ),
-//   cases: {
-//     loading: () => <Spin size="large" />,
-//     empty: () => <Empty description="No tasks found" />,
-//     ready: TasksList,
-//   },
-//   hooks: {
-//     mounted: taskModel.effects.getTasksListFx.prepend(() => {}),
-//   },
-// });
+
+const TasksList = list({
+  view: ListItemView,
+  source: taskModel.$tasksFiltered,
+  bind: {},
+  mapItem: {
+    task: (task) => task,
+  },
+});
+
+const PageContent = variant({
+  source: combine(
+    {
+      isLoading: taskModel.$tasksListLoading,
+      isEmpty: taskModel.$tasksListEmpty,
+    },
+    ({ isLoading, isEmpty }) => {
+      if (isLoading) return "loading";
+      if (isEmpty) return "empty";
+      return "ready";
+    }
+  ),
+  cases: {
+    loading: () => <Spin size="large" />,
+    empty: () => <Empty description="No tasks found" />,
+    ready: TasksList,
+  },
+  hooks: {
+    mounted: taskModel.effects.getTasksListFx.prepend(() => {}),
+  },
+});
